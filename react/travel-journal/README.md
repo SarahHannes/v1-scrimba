22f901f0f506775cb243e293e76aff3904a01ea9
sarah-hannes <siti.sarah.a.hannes@gmail.com>
2025-03-27 16:42:05 +0800

    update: change directories

--- react/travel-journal/README.md
+++
@@ -1,8 +0,0 @@
-# Travel Journal 🚃
-
-A simple travel blog using React ⚛️ !
-
-![Sreenshot Desktop](screenshot/Desktop.png)
-
-# Credit
-- The course: https://scrimba.com/learn-react-c0e
--- react/travel-journal/src/components/Header.jsx
+++
@@ -1,8 +0,0 @@
-export default function Header() {
-    return (
-        <header className="header">
-            <img className="globe-icon" src="./images/globe.png" alt="Globe icon"/>
-            <p className="blogname">my travel journal.</p>
-        </header>
-    )
-}
